4880d031f0fadc8bd679c64b68ce90fa609d7a42
Jan Mirco Pfeifer <126580858+janmirco@users.noreply.github.com>
2025-05-09 12:06:21 +0200

    feat(path): add sub dirs with date time names

--- rippl/path.py
+++ rippl/path.py
@@ -1,8 +1,9 @@
+from datetime import datetime
 from pathlib import Path
 
 
-def set_up(output_dir_name: str = "output") -> Path:
-    output_dir = Path.cwd() / Path(output_dir_name)
-    if not output_dir.exists():
-        output_dir.mkdir()
+def set_up_output(output_root: Path = Path.cwd() / Path("output")) -> Path:
+    current_time = datetime.now().isoformat()
+    output_dir = output_root / Path(current_time)
+    output_dir.mkdir(parents=True)
     return output_dir
