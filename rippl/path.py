e5096b4556c38358764a802427e7e50317f384fb
Jan Mirco Pfeifer <126580858+janmirco@users.noreply.github.com>
2025-05-09 12:10:33 +0200

    feat(path): add check for possible data dir

--- rippl/path.py
+++ rippl/path.py
@@ -7,3 +7,9 @@ def set_up_output(output_root: Path = Path.cwd() / Path("output")) -> Path:
     output_dir = output_root / Path(current_time)
     output_dir.mkdir(parents=True)
     return output_dir
+
+
+def set_up_data(data_dir: Path) -> Path:
+    if not data_dir.exists():
+        raise FileNotFoundError(f"Data path does not exist: {data_dir.as_posix()}")
+    return data_dir
